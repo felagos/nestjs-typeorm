362c8b5385b5bad87e238b0be4537b79c24f7a8b
Felipe Lagos <felagossanchez@gmail.com>
2023-09-12 08:13:06 -0300

    new atributtes product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -5,9 +5,28 @@ export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({
+  @Column('varchar', {
+    name: 'title',
     length: 250,
     unique: true,
   })
   title: string;
+
+  @Column('numeric', { name: 'price' })
+  price: number;
+
+  @Column('varchar', { name: 'description', length: 250, nullable: true })
+  description: number;
+
+  @Column('varchar', { name: 'slug', length: 250, unique: false })
+  slug: number;
+
+  @Column('int', { name: 'stock', default: 0 })
+  stock: number;
+
+  @Column('varchar', { array: true, length: 10 })
+  size: string[];
+
+  @Column('varchar', { name: 'gender', length: 10 })
+  gender: number;
 }
